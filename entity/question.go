53049a4ae106a4f405d5f60974818e47389d1f13
gloonch <mahdihadian@hotmail.com>
2024-07-15 16:50:10 +0330

    define core entities

--- entity/question.go
+++ entity/question.go
@@ -2,17 +2,17 @@ package entity
 
 type Question struct {
 	ID              uint
-	Question        string
+	Text            string
 	PossibleAnswers []PossibleAnswer
 	CorrectAnswerID uint
-	Difficulty      string
+	Difficulty      QuestionDifficulty
 	CategoryID      uint
 }
 
 type PossibleAnswer struct {
-	ID      uint
-	Content string
-	Choice  PossibleAnswerChoice
+	ID     uint
+	Text   string
+	Choice PossibleAnswerChoice
 }
 
 type PossibleAnswerChoice uint8
@@ -30,3 +30,19 @@ const (
 	PossibleAnswerC
 	PossibleAnswerD
 )
+
+type QuestionDifficulty uint8
+
+const (
+	QuestionDifficultyEasy QuestionDifficulty = iota + 1
+	QuestionDifficultyMedium
+	QuestionDifficultyHard
+)
+
+func (q QuestionDifficulty) IsValid() bool {
+	if q >= QuestionDifficultyEasy && q <= QuestionDifficultyHard {
+		return true
+	}
+
+	return false
+}

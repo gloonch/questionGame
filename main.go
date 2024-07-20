733836ac969d09e9b6b58f5d867e1d7ecc4454d7
gloonch <mahdihadian@hotmail.com>
2024-07-20 19:34:35 +0330

    register user api (in main)

--- main.go
+++ main.go
@@ -1,33 +1,95 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"questionGame/entity"
+	"io"
+	"log"
+	"net/http"
 	"questionGame/repository/mysql"
+	"questionGame/service/userservice"
 )
 
 func main() {
-	testUserMysqlRepo()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health-check", healthCheckHandler)
+	mux.HandleFunc("/users/register", userRegisterHandler)
+
+	log.Println("server is listening on port 8080...")
+	server := http.Server{Addr: ":8080", Handler: mux}
+	log.Fatal(server.ListenAndServe())
 }
 
-func testUserMysqlRepo() {
+func userRegisterHandler(writer http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		fmt.Fprintf(writer, `{"error": "invalid method"}`)
+	}
+
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		writer.Write([]byte(
+			fmt.Sprintf(`{"error": "%s"}`, err.Error()),
+		))
+
+		return
+	}
+
+	var uReq userservice.RegisterRequest
+	err = json.Unmarshal(data, &uReq)
+	if err != nil {
+		writer.Write([]byte(
+			fmt.Sprintf(`{"error": "%s"}`, err.Error()),
+		))
+
+		return
+	}
 
 	mysqlRepo := mysql.New()
+	userSvc := userservice.New(mysqlRepo)
 
-	createdUser, err := mysqlRepo.Register(entity.User{
-		ID:          0,
-		PhoneNumber: "0912",
-		Name:        "Mahdi Hadian",
-	})
+	_, err = userSvc.Register(uReq)
 	if err != nil {
-		fmt.Println("register user: ", err)
-	} else {
-		fmt.Println("created user: ", createdUser)
+		writer.Write([]byte(
+			fmt.Sprintf(`{"error": "%s"}`, err.Error()),
+		))
+
+		return
 	}
 
-	isUnique, err := mysqlRepo.IsPhoneNumberUnique(createdUser.PhoneNumber)
-	if err != nil {
-		fmt.Println("unique err")
-	}
-	fmt.Println("isUnique: ", isUnique)
+	writer.Write([]byte(`{"message": "user created successfully"}`))
+
 }
+
+func healthCheckHandler(writer http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(writer, `{"message": "everything is ok"}`)
+}
+
+//func testUserMysqlRepo() {
+//
+//	mysqlRepo := mysql.New()
+//
+//	mysql.New()
+//	mysql.MySQLDB{}
+//	inja faqat db.go ro migire
+//
+//	vali az mysqlRepo mituni method haro call koni ke tuye user.go e
+//	mysqlRepo.IsPhoneNumberUnique()
+//
+//	createdUser, err := mysqlRepo.Register(entity.User{
+//		ID:          0,
+//		PhoneNumber: "0912",
+//		Name:        "Mahdi Hadian",
+//	})
+//	if err != nil {
+//		fmt.Println("register user: ", err)
+//	} else {
+//		fmt.Println("created user: ", createdUser)
+//	}
+//
+//	isUnique, err := mysqlRepo.IsPhoneNumberUnique(createdUser.PhoneNumber)
+//	if err != nil {
+//		fmt.Println("unique err")
+//	}
+//	fmt.Println("isUnique: ", isUnique)
+//}

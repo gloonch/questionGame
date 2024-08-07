9086a40a058b405f59195560415c77fc3fa17726
gloonch <mahdihadian@hotmail.com>
2024-08-07 19:57:38 +0330

    replaced all net.http handlers with echo handlers

--- delivery/httpserver/user_service.go
+++ delivery/httpserver/user_service.go
@@ -6,6 +6,21 @@ import (
 	"questionGame/service/userservice"
 )
 
+func (s Server) userLogin(c echo.Context) error {
+
+	var req userservice.LoginRequest
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+
+	response, lErr := s.userSvc.Login(req)
+	if lErr != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, lErr.Error())
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
+
 func (s Server) userRegister(c echo.Context) error {
 
 	var uReq userservice.RegisterRequest
@@ -19,4 +34,20 @@ func (s Server) userRegister(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, resp)
+}
+
+func (s Server) userProfile(c echo.Context) error {
+	authToken := c.Request().Header.Get("Authorization")
+	claims, err := s.authSvc.ParseToken(authToken)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+	}
+
+	response, err := s.userSvc.GetProfile(userservice.ProfileRequest{UserID: claims.UserID})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, response)
+
 }
